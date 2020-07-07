a9d1dda8009f4747115fa8f1e868f95436516c51
Joseph Mawer <joseph.mawer@caseware.com>
2020-07-07 08:06:23 -0400

    Adding the dialog and sides to menu items

---
+++ CrowBar/Models/MenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CrowBar.Models
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string GetFormattedBasePrice()
+        {
+            return "9.99";
+        }
+        public List<SideMenuItem> Sides = new List<SideMenuItem>();
+    }
+}
---
+++ CrowBar/Models/SideMenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace CrowBar.Models
+{
+    public class SideMenuItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+    }
+}
